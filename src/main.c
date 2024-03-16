4b86f9672e5a4749e7db1e3ed4e94690f677b2e2
Adam JOLY <adjoly@student.42angouleme.fr>
2024-03-16 20:08:56 +0100

    finished... supposedly

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: adjoly <adjoly@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 12:14:22 by adjoly            #+#    #+#             */
-/*   Updated: 2024/03/13 14:44:42 by adjoly           ###   ########.fr       */
+/*   Updated: 2024/03/16 20:07:36 by adjoly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,14 @@ void	check_invalid_char(char **av)
 		tmp_args = *tmp;
 		while (*tmp_args)
 		{
-			if (!ft_isdigit(*tmp_args) && *tmp_args != '-' && *tmp_args != '+' && *tmp_args != 32)
+			if (!ft_isdigit(*tmp_args) && *tmp_args != '-' \
+					&& *tmp_args != '+' && *tmp_args != 32)
 				ft_senderror();
 			tmp_args++;
 		}
 		tmp++;
 	}
 }
-
 
 void	check_double(t_stack **stack)
 {
@@ -55,6 +55,53 @@ void	check_double(t_stack **stack)
 	}
 }
 
+void	check_sign(char **av)
+{
+	char	**tmp;
+	char	*tmp_av;
+
+	tmp = av;
+	tmp++;
+	while (*tmp)
+	{
+		tmp_av = *tmp;
+		while (*tmp_av)
+		{
+			if ((*tmp_av == '-' || *tmp_av == '+') \
+					&& !ft_isdigit(*(tmp_av + 1)))
+				ft_senderror();
+			if ((*tmp_av == '-' || *tmp_av == '+') && ft_isdigit(*(tmp_av - 1)))
+				ft_senderror();
+			tmp_av++;
+		}
+		tmp++;
+	}
+}
+
+void	check_empty_args(char **av)
+{
+	char	**tmp;
+	char	*tmp_av;
+
+	tmp = av;
+	tmp++;
+	while (*tmp)
+	{
+		tmp_av = *tmp;
+		if (!*tmp_av)
+			ft_senderror();
+		while (*tmp_av)
+		{
+			if (*tmp_av != 32)
+				break ;
+			tmp_av++;
+		}
+		if (!*tmp_av)
+			ft_senderror();
+		tmp++;
+	}
+}
+
 int	main(int ac, char **av)
 {
 	t_stack	*stack_a;
@@ -62,7 +109,9 @@ int	main(int ac, char **av)
 
 	if (ac < 2)
 		return (EXIT_SUCCESS);
+	check_empty_args(av);
 	check_invalid_char(av);
+	check_sign(av);
 	stack_b = NULL;
 	stack_a = ft_parsing(av);
 	if (ft_stacksize(stack_a) <= 1)
--- src/utils/print_error.c
+++ src/utils/print_error.c
@@ -6,7 +6,7 @@
 /*   By: adjoly <adjoly@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 15:47:05 by adjoly            #+#    #+#             */
-/*   Updated: 2024/03/11 15:54:09 by adjoly           ###   ########.fr       */
+/*   Updated: 2024/03/16 19:01:15 by adjoly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,6 @@
 
 void	ft_senderror(void)
 {
-	ft_putendl_fd("Error", STDOUT_FILENO);
+	ft_putendl_fd("Error", STDERR_FILENO);
 	exit(EXIT_FAILURE);
 }
